5d89dff665dbde82a46c14a88b435f991cc8cbd6
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-03-13 12:09:46 -0300

    feat: add reset css and material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {GlobalStyles} from './styles/GlobalStyles';
+
 import {App} from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
