eb045c7a57bdca2fc6bfc77197f910542a96583d
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-03-13 15:38:58 -0300

    feat: login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import { TextField,Button } from '@mui/material';
+
+import {UserService} from 'shared/services';
 
 import * as S from './styles';
 
 export const Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = async() =>{
+    const userService = new UserService();
+
+    await userService.authenticate('','');
+
+    navigate('/');
+  };
+
   return (
     <S.Container>
       <S.Logo/>
@@ -15,7 +28,7 @@ export const Login: React.FC = () => {
           <TextField className='txt-login' type="password" label="Senha" variant="outlined" />
           <TextField className='txt-login' type="password" label="Confirmar Senha" variant="outlined" />
           <div className='wrapper-button'>
-            <Button variant="contained">Entrar</Button>
+            <Button variant="contained" onClick={handleLogin}>Entrar</Button>
           </div>
         </S.Form>
       </S.Content>
